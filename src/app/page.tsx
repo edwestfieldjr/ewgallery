7e82f061e6582c1f32cd6d3bcd40210d53ccc21d
Ed Westfield Jr. 西田爱德 <30934590+edwestfieldjr@users.noreply.github.com>
2024-05-19 15:50:19 -0400

    conform code to tutorial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import Link from "next/link";
 import { headers } from "next/headers";
 import { SignedIn, SignInButton, SignedOut } from "@clerk/nextjs";
-import { Images } from "~/app/_components/images";
+import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
+
+async function Images() {
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+  return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div key={image.id} className="w-48">
+          <img src={image.url} alt={image.name} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default async function HomePage() {
   headers();
