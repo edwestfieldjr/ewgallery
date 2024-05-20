4e304655f7a237987ac1045640b297c85d76dd70
Ed Westfield Jr. 西田爱德 <30934590+edwestfieldjr@users.noreply.github.com>
2024-05-19 20:56:23 -0400

    breakout queries into separate file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import Link from "next/link";
 import { headers } from "next/headers";
 import { SignedIn, SignInButton, SignedOut } from "@clerk/nextjs";
-import { db } from "~/server/db";
+import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
 
 async function Images() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
+  const images = await getMyImages();
+
   return (
     <div className="flex flex-wrap gap-4">
       {images.map((image) => (
